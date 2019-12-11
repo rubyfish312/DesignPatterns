1d426c50e0c2fa2459e549c028a4a533eac49e01
rubyfish312 <32665025+rubyfish312@users.noreply.github.com>
2019-12-11 14:17:52 +0800

    PatternForPython

---
+++ ObserverPattern/p1_1.py
@@ -0,0 +1,27 @@
+#p1_1
+from abc import ABCMeta, abstractmethod
+
+#%%
+class WaterHeater:
+
+    def __init__(self):
+        self._observers = []
+        self._temperature = 25
+    
+    def getTemperature(self):
+        return self._temperature
+
+    def setTemperature(self, temperature):
+        self._temperature = temperature
+        print("current temperature is : " + str(self._temperature) + " ")
+        self.notifies()
+
+    def addObserver(self, observer):
+        self._observers.append(observer)
+
+    def notifies(self):
+        for o in self._observers:
+            o.update(self)
+
+
+# %%
