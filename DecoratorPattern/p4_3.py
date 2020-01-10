6719839347b151d436509f5bbf7883c38e8ee46f
rubyfish312 <32665025+rubyfish312@users.noreply.github.com>
2020-01-10 10:11:50 +0800

    Update p4_3.py

--- DecoratorPattern/p4_3.py
+++ DecoratorPattern/p4_3.py
@@ -1,22 +1,52 @@
 #%%
 import logging
-
-#%%
 logging.basicConfig(level=logging.INFO)
-
+#%%
 def loggingDecorator(func):
     """decorator of write log"""
     def wrapperLogging(*args, **kwargs):
-        logging.info("start %s() ..." % func.__name__)
+        logging.info(" start %s() ..." % func.__name__)
         func(*args, **kwargs)
-        logging.info("%s() finish" % func.__name__)
+        logging.info(" %s() finish" % func.__name__)
     return wrapperLogging
 
+#%%
 def showInfo(*args, **kwargs):
-    print(" this is a testing func : ", args, kwargs)
+    print("this is a testing func with args : ", args, kwargs)
 
-decoratedShow = loggingDecorator(showInfo)
-decoratedShow('arg1','arg2',kwarg1 = 1, kwarg2 = 2)
+#%%
+# showInfo
+# <function __main__.showInfo(*args, **kwargs)>
+
+# showInfo()
+# this is a testing func :  () {}
+
+# showInfo('arg1','arg2',kwarg1 = 1, kwarg2 = 2)
+# this is a testing func :  ('arg1', 'arg2') {'kwarg1': 1, 'kwarg2': 2}
+
+#%%
+# loggingDecorator(showInfo)
+# <function __main__.loggingDecorator.<locals>.wrapperLogging(*args, **kwargs)>
+
+# loggingDecorator(showInfo())
+# this is a testing func :  () {}
+# <function __main__.loggingDecorator.<locals>.wrapperLogging(*args, **kwargs)>
+
+# loggingDecorator(showInfo)('arg1','arg2',kwarg1 = 1,kwarg = 2)
+# INFO:root: start showInfo() ...
+# this is a testing func with args :  () {}
+# INFO:root: showInfo() finish
+
+# loggingDecorator(showInfo())()
+# decoratedShowInfo = loggingDecorator(showInfo)
+# AttributeError: 'NoneType' object has no attribute '__name__'
+
+#%%
+#decoratedShowInfo = loggingDecorator(showInfo)
+#decoratedShowInfo('arg1','arg2',kwarg1 = 1,kwarg = 2)
+# INFO:root: start showInfo() ...
+# this is a testing func with args :  ('arg1', 'arg2') {'kwarg1': 1, 'kwarg': 2}
+# INFO:root: showInfo() finish
 
 # %%
 # def showMin(a, b):
@@ -27,10 +57,10 @@ decoratedShow('arg1','arg2',kwarg1 = 1, kwarg2 = 2)
 
 
 # %%
-@loggingDecorator
-def showMin(a, b):
-    print("%d, %d the min in %d" % (a,b,a+b))
+# @loggingDecorator
+# def showMin(a, b):
+#     print("%d, %d the min in %d" % (a,b,a+b))
 
-showMin(2, 3)    
+#showMin(2, 3)    
 
 # %%
